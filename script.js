ce6106d48e6a59e1a4dca068ae6f1afb5008eca0
sthephaniplatero <pla21002@byui.edu>
2025-07-25 13:26:10 -0600

    Actualizar carrito

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 
-//Funcion para mostrar carrito
+//Funcion para mostrar carrito y agregar overlay completo.
+const sidebar = document.getElementById("carritoSidebar");
+const overlay = document.getElementById("overlay");
 
 function mostrarCarritoSidebar() {
-    const sidebar = document.getElementById("carritoSidebar");
-    sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+    const carritoVisible = sidebar.classList.contains("activa");
 
+    if (carritoVisible) {
+        sidebar.classList.remove("activa");
+        overlay.style.display = "none";
+    } else {
+        sidebar.classList.add("activa");
+        overlay.style.display = "block";
+    }
 }
 
+// Cierra el carrito si se hace clic en el fondo oscuro
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("activa");
+    overlay.style.display = "none";
+});
+
+//Funcion para actualizar carrito.
 
 let carrito = [];
 
@@ -32,6 +47,7 @@ function actualizarCarrito() {
     totalCarrito.textContent = `Total: $${total.toFixed(2)}`;
 }
 
+//Funcion para agregar elementos al carrito incluyendo el precio y sumar el total.
 
 function agregarAlCarrito(nombre, precio) {
     const productoExistente = carrito.find(item => item.nombre === nombre);
@@ -45,7 +61,31 @@ function agregarAlCarrito(nombre, precio) {
     actualizarCarrito();
 }
 
+
+//Funcion para eliminar los elementos del carrito
+
 function eliminarDelCarrito(nombre) {
     carrito = carrito.filter(item => item.nombre !== nombre);
     actualizarCarrito();
+}
+
+
+//Funcion para funcionalidad del boton finalizar compra y mostrar mensaje de compra procesada.
+
+function finalizarCompra() {
+  sidebar.classList.remove("activa");
+  overlay.style.display = "none";
+
+  carrito = [];
+  actualizarCarrito();
+
+  // Mostrar mensaje
+  const mensajeGeneral = document.getElementById("mensaje-general");
+  mensajeGeneral.textContent = "¡Compra procesada con éxito!";
+  mensajeGeneral.style.display = "block";
+
+  setTimeout(() =>{
+    mensajeGeneral.style.display = "none";
+    mensajeGeneral.textContent = "";
+  }, 3000);
 }
